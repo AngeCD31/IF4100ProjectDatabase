8acdded75c756f54129dcc3aa31ed8634aa2b78c
Yulissa1633 <yulissa@1633gmail.com>
2020-11-15 21:08:13 -0600

    Start UI

--- src/main/java/edu/ucr/rp/db/ui/App.java
+++ src/main/java/edu/ucr/rp/db/ui/App.java
@@ -1,4 +1,19 @@
 package edu.ucr.rp.db.ui;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        //stage.resizableProperty().setValue(Boolean.FALSE);
+    }
 }
---
+++ src/main/java/edu/ucr/rp/db/ui/Home.java
@@ -0,0 +1,53 @@
+package edu.ucr.rp.db.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+public class Home {
+
+    @FXML private TabPane tP_Tables;
+    @FXML private Tab tb_TableOne;
+    @FXML private Tab tb_TableTwo;
+    @FXML private Tab tb_TableThree;
+    @FXML private Tab tb_TableFour;
+    @FXML private Tab tb_TableFive;
+
+    @FXML
+    private void TableOneAction(ActionEvent event) {
+
+
+
+    }
+
+    @FXML
+    private void TableTwoAction(ActionEvent event) {
+
+
+
+    }
+
+    @FXML
+    private void TableThreeAction(ActionEvent event) {
+
+
+
+    }
+
+    @FXML
+    private void TableFourAction(ActionEvent event) {
+
+
+
+    }
+
+    @FXML
+    private void TableFiveAction(ActionEvent event) {
+
+
+
+    }
+
+}
